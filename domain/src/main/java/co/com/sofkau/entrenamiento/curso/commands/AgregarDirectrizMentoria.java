47988bf4d0440ea14c5ed647400e0aae07215de5
Juanes07 <juanesdm3@gmail.com>
2022-05-16 15:25:47 -0500

    Caso de uso AgregarDirectrizMentoriaUseCase
    Se agrega el caso de uso para directriz, agregando las caracteristicas faltantes al constructor de AgregarDirectrizMentoria

--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizMentoria.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizMentoria.java
@@ -1,16 +1,19 @@
 package co.com.sofkau.entrenamiento.curso.commands;
 
 import co.com.sofka.domain.generic.Command;
+import co.com.sofkau.entrenamiento.curso.values.CursoId;
 import co.com.sofkau.entrenamiento.curso.values.Directiz;
 import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
 
 public class AgregarDirectrizMentoria extends Command {
     private final MentoriaId mentoriaId;
     private final Directiz directiz;
+    private final CursoId cursoId;
 
-    public AgregarDirectrizMentoria(MentoriaId mentoriaId, Directiz directiz) {
+    public AgregarDirectrizMentoria(MentoriaId mentoriaId, Directiz directiz, CursoId cursoId) {
         this.mentoriaId = mentoriaId;
         this.directiz = directiz;
+        this.cursoId = cursoId;
     }
 
     public MentoriaId getMentoriaId() {
@@ -19,5 +22,9 @@ public class AgregarDirectrizMentoria extends Command {
 
     public Directiz getDirectiz() {
         return directiz;
+    }
+
+    public CursoId getCursoId() {
+        return cursoId;
     }
 }
--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/values/Directiz.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/values/Directiz.java
@@ -1,6 +1,7 @@
 package co.com.sofkau.entrenamiento.curso.values;
 
 import co.com.sofka.domain.generic.ValueObject;
+import co.com.sofkau.entrenamiento.curso.Curso;
 
 public class Directiz implements ValueObject<String> {
     @Override
